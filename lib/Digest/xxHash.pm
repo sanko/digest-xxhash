d3e96b4db78bc6c2a442f4ab77a7f629cee84333
Sanko Robinson <sanko@cpan.org>
2015-11-04 15:57:36 -0500

    Add 64bit hash functions and remove object oriented API

--- lib/Digest/xxHash.pm
+++ lib/Digest/xxHash.pm
@@ -4,14 +4,16 @@ use base qw(Exporter);
 use Config ();
 use XSLoader;
 BEGIN {
-    our $VERSION = '1.02';
+    our $VERSION = '2.00';
     XSLoader::load __PACKAGE__, $VERSION;
 }
 
-our @EXPORT_OK = qw[xxhash xxhash_hex];
+our @EXPORT_OK = qw[xxhash32 xxhash32_hex
+					xxhash64 xxhash64_hex];
 
-sub xxhash_hex{unpack('H8', pack('N', xxhash(@_)))}
-sub digest_hex{unpack('H8', pack('N', +shift->digest( )))}
+sub xxhash32_hex{unpack('H*', pack('N', xxhash32(@_)))}
+sub xxhash64_hex{unpack('H*', pack('N', xxhash64(@_)))}
+
 1;
 __END__
 
@@ -21,64 +23,39 @@ Digest::xxHash - xxHash Implementation For Perl
 
 =head1 SYNOPSIS
 
-    use Digest::xxHash qw[xxhash xxhash_hex];
+    use Digest::xxHash qw[xxhash32 xxhash32_hex];
 
-    my $hash = xxhash( $data, $seed );
-    my $hex  = xxhash_hex( $data, $seed );
+    my $hash = xxhash32( $data, $seed );
+    my $hex  = xxhash32_hex( $data, $seed );
 
-    # OO remix!
-
-    my $digest = Digest::xxHash->new( $seed );
-    $digest->add( $data );
-    $digest->add( $more_data );
-    my $xxhash = $digest->digest( );
-    my $xxhex  = $digest->digest_hex( );
+    my $hash_64 = xxhash64( $data, $seed );
+    my $hex_64  = xxhash64_hex( $data, $seed );
 
 =head1 DESCRIPTION
 
-This module provides both a functional and an OO interface to xxHash
-functions. xxHash is an extremely fast algorithm that claims to work at speeds
-close to RAM limits.
+This module provides both a functional and an object oriented interface to
+xxHash functions. xxHash is an extremely fast algorithm that claims to work at
+speeds close to RAM limits.
 
 =head1 FUNCTIONAL INTERFACE
 
 These functions are easy to use but aren't very flexible.
 
-=head2 $h = xxhash( $data, $seed )
+=head2 $h = xxhash32( $data, $seed )
 
 Calculates a 32 bit hash.
 
-=head2 $h = xxhash_hex( $data, $seed )
+=head2 $h = xxhash32_hex( $data, $seed )
 
 Calculates a 32 bit hash and returns it as a hex string.
 
-=head1 OO INTERFACE
+=head2 $h = xxhash64( $data, $seed )
 
-The OO interface is a lot more flexible but less succinct.
+Calculates a 64 bit hash.
 
-=head2 $d = Digest::xxHash->new( $seed )
+=head2 $h = xxhash64_hex( $data, $seed )
 
-Returns a new Digest::xxHash object. This is a simple constructor.
-
-=head2 $d->add( $data )
-
-Adds data to the spool that will eventually be hashed. You may call this as
-many times as needed.
-
-=head2 $d->reset( )
-
-This will reset the data L<added|/"$d-E<< gt >>add( $data )"> to the object.
-This is great if you want to hash several pieces of data with the same seed.
-
-=head2 $h = $d->digest( )
-
-Calculates a 32 bit hash of the data L<added|/"$d-E<< gt >>add( $data )"> to
-the object.
-
-=head2 $h = $d->digest_hex( )
-
-Calculates a 32 bit hash of the data L<added|/"$d-E<< gt >>add( $data )"> to
-the object and returns it as a hex string.
+Calculates a 64 bit hash and returns it as a hex string.
 
 =head1 SPEED
 
@@ -88,15 +65,17 @@ like:
 
     Name            Speed       Q.Score   Author
     xxHash          5.4 GB/s     10
-    MumurHash 3a    2.7 GB/s     10       Austin Appleby
-    SpookyHash      2.0 GB/s     10       Bob Jenkins
-    SBox            1.4 GB/s      9       Bret Mulvey
-    Lookup3         1.2 GB/s      9       Bob Jenkins
-    CityHash64      1.05 GB/s    10       Pike & Alakuijala
-    FNV             0.55 GB/s     5       Fowler, Noll, Vo
-    CRC32           0.43 GB/s     9
-    MD5-32          0.33 GB/s    10       Ronald L. Rivest
-    SHA1-32         0.28 GB/s    10
+	CrapWow         3.2 GB/s      2       Andrew
+	MumurHash 3a    2.7 GB/s     10       Austin Appleby
+	SpookyHash      2.0 GB/s     10       Bob Jenkins
+	SBox            1.4 GB/s      9       Bret Mulvey
+	Lookup3         1.2 GB/s      9       Bob Jenkins
+	SuperFastHash   1.2 GB/s      1       Paul Hsieh
+	CityHash64      1.05 GB/s    10       Pike & Alakuijala
+	FNV             0.55 GB/s     5       Fowler, Noll, Vo
+	CRC32           0.43 GB/s     9
+	MD5-32          0.33 GB/s    10       Ronald L. Rivest
+	SHA1-32         0.28 GB/s    10
 
 Q.Score is a measure of "quality" of the hash function. It depends on
 successfully passing
@@ -104,11 +83,18 @@ L<SMHasher test set|http://code.google.com/p/smhasher/wiki/SMHasher>. 10 is a
 perfect score. Hash functions with a Q.score E<lt> 5 are not listed in this
 table.
 
+A 64-bits version, named XXH64, is available since (upstream) r35.
+It offers much better speed, but for 64-bits applications only.
+	
+	Name     Speed on 64 bits    Speed on 32 bits
+	XXH64       13.8 GB/s            1.9 GB/s
+	XXH32        6.8 GB/s            6.0 GB/s
+
 =head1 LICENSE
 
-xxxHash is covered by the BSD license.
+xxHash is covered by the BSD license.
 
-I don't actually care about the wrapper I've written.
+License-wise, I don't actually care about the wrapper I've written.
 
 =head1 AUTHOR
 
--- xxHash.xs
+++ xxHash.xs
@@ -2,6 +2,8 @@
 #include "perl.h"
 #include "XSUB.h"
 
+#if(IVSIZE < 8)
+#include "perl_math_int64.h"
 #else
 #define SvI64 SvIV
 #define newSVi64 newSViv
@@ -15,66 +17,20 @@ MODULE = Digest::xxHash  PACKAGE = Digest::xxHash
 
 TYPEMAP: <<HERE
 
-U64           T_UV
-
 unsigned long long         T_UV
 
 HERE
 
- 
 U32
-xxhash( const char *input, int length(input), UV seed )
+xxhash32( const char *input, int length(input), UV seed )
     CODE:
         RETVAL = XXH32(input, STRLEN_length_of_input, seed);
     OUTPUT:
         RETVAL
 
-void *
-xxHash::new( UV seed )
-    PPCODE:
-        RETVAL = XXH32_init(seed);
-        ST(0) = sv_newmortal();
-        sv_setref_pv(ST(0), CLASS, (void*)RETVAL);
-        XSRETURN(1);
-
-void
-add( self, const char *input, int length(input) )
-    PREINIT:
-        void * state;
-    PPCODE:
-        if (SvROK(ST(0)) && sv_derived_from(ST(0), "Digest::xxHash")) {
-            IV tmp = SvIV((SV*)SvRV(ST(0)));
-            state = INT2PTR(void *,tmp);
-        }
-        else
-            Perl_croak(aTHX_ "%s: %s is not of type %s", "Digest::xxHash::add", "self", "Digest::xxHash");
-        XXH32_update(state, input, STRLEN_length_of_input );
-
-U32
-digest( self, ... )
-    PREINIT:
-        void * state;
-    PPCODE:
-        if (SvROK(ST(0)) && sv_derived_from(ST(0), "Digest::xxHash")) {
-            IV tmp = SvIV((SV*)SvRV(ST(0)));
-            state = INT2PTR(void *,tmp);
-        }
-        else
-            Perl_croak(aTHX_ "%s: %s is not of type %s", "Digest::xxHash::add", "self", "Digest::xxHash");
-         RETVAL = XXH32_intermediateDigest(state);
-         XSprePUSH; PUSHu((UV)RETVAL);
-         XSRETURN(1);
-
-void
-reset( self )
-    PREINIT:
-        void * state;
-    PPCODE:
-        if (SvROK(ST(0)) && sv_derived_from(ST(0), "Digest::xxHash")) {
-            IV tmp = SvIV((SV*)SvRV(ST(0)));
-            state = INT2PTR(void *,tmp);
-        }
-        else
-            Perl_croak(aTHX_ "%s: %s is not of type %s", "Digest::xxHash::add", "self", "Digest::xxHash");
-        struct XXH_state32_t * _state = (struct XXH_state32_t *) state;
-        XXH32_resetState(state, _state->seed);
+unsigned long long
+xxhash64( const char *input, int length(input), UV seed )
+    CODE:
+        RETVAL = XXH64(input, STRLEN_length_of_input, seed);
+    OUTPUT:
+        RETVAL
